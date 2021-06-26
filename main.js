84d28f1a5bb28166d62eb426d04686fafdbb34aa
joolim <76763957+joolim@users.noreply.github.com>
2021-06-26 18:09:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onsize = screen;
+window.onload = screen;
+
+
+function screen() {
+    Width = window.innerWidth;
+
+    document.getElementById('size').innerHTML
+    = "Width: " + Width + "px" ;
+}
